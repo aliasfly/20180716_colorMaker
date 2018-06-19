87f5cce58b540fc288127b8b016c3c13065e43b0
aliasfly <zhangzexiangks@126.com>
2018-06-19 11:46:25 +0800

    have some build problem need to add more code

--- dataPlot.h
+++ dataPlot.h
@@ -15,23 +15,23 @@ QT_CHARTS_USE_NAMESPACE
 class DataPlot: public QObject
 {
     Q_OBJECT
-//    Q_PROPERTY(QAbstractAxis* axisX READ axisX WRITE setAxisX)
-//    Q_PROPERTY(QAbstractAxis* axisY READ axisY WRITE setAxisY)
+    Q_PROPERTY(QAbstractAxis* axisX READ axisX WRITE setAxisX)
+    Q_PROPERTY(QAbstractAxis* axisY READ axisY WRITE setAxisY)
 public:
     DataPlot(QObject *parent = nullptr);
     ~DataPlot();
 
-//    QAbstractAxis* axisX() const { return _axisX; }
-//    QAbstractAxis* axisY() const { return _axisY; }
+    QAbstractAxis* axisX() const { return _axisX; }
+    QAbstractAxis* axisY() const { return _axisY; }
 
 
-//    void setAxisX(QAbstractAxis* axisX);
-//    void setAxisY(QAbstractAxis* axisY);
+    void setAxisX(QAbstractAxis* axisX);
+    void setAxisY(QAbstractAxis* axisY);
 
 
-//public slots:
-//    void seriesAdded(QAbstractSeries* series);
-//    void seriesRemoved(QAbstractSeries* series);
+public slots:
+    void seriesAdded(QAbstractSeries* series);
+    void seriesRemoved(QAbstractSeries* series);
 
 private:
     QAbstractAxis*   _axisX;
