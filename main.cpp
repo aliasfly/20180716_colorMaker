78243da2df96dca07076b40f165b64308bf6944d
aliasfly <zhangzexiangks@126.com>
2018-06-19 17:01:03 +0800

    can plot

--- main.cpp
+++ main.cpp
@@ -5,19 +5,29 @@
 #include "dataStore.h"
 #include "dataPlot.h"
 
-
+#include <QtWidgets/QApplication>
+#include <QtCore/QDir>
+#include <QtQuick/QQuickView>
+#include <QtQml/QQmlEngine>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    QApplication app(argc, argv);
 
     QQuickView viewer;
-    viewer.setResizeMode(QQuickView::SizeRootObjectToView);//default
+#ifdef Q_OS_WIN
+    QString extraImportPath(QStringLiteral("%1/../../../../%2"));
+#else
+    QString extraImportPath(QStringLiteral("%1/../../../%2"));
+#endif
+    viewer.engine()->addImportPath(extraImportPath.arg(QGuiApplication::applicationDirPath(),
+                                      QString::fromLatin1("qml")));
     QObject::connect(viewer.engine(), SIGNAL(quit()), &app, SLOT(quit()));//quit_use
     viewer.rootContext()->setContextProperty("colorMaker", new ColorMaker);
     viewer.rootContext()->setContextProperty("dataStore", new DataStore);
     viewer.rootContext()->setContextProperty("dataPlot", new DataPlot);
     viewer.setSource(QUrl("qrc:///main.qml"));
+    viewer.setResizeMode(QQuickView::SizeRootObjectToView);//default
     viewer.show();
 
     return app.exec();
