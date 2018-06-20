443d6dbf9c2f55181e066e5f1a0e7f44d17c7041
aliasfly <zhangzexiangks@126.com>
2018-06-20 11:53:29 +0800

    ideal need to solve

--- dataPlot.cpp
+++ dataPlot.cpp
@@ -39,7 +39,7 @@ void DataPlot::produceXYvalue()
 {
     for(int i=0;i<10;i++)
     {
-        DataPlot::setXvalue(i);
+//        DataPlot::setXvalue(i);
 //        DataPlot::setYvalue(i);
     }
 }
